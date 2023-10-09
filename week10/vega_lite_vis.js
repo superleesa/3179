29fc4a73091f4afc25544ad48f5ab27d1e0b8b23
Satoshi <skas0016@student.monash.edu>
2023-10-09 17:32:01 +0800

    added week10 visualizations

---
+++ week10/vega_lite_vis.js
@@ -0,0 +1,8 @@
+const vg_1 = "choropleth.json";
+const vg_2 = "bump_chart.json";
+const vg_3 = "line_chart.json";
+
+
+vegaEmbed("#minimum_wage_map", vg_1).catch(console.error);
+vegaEmbed("#avg_wage_bump", vg_2).catch(console.error);
+vegaEmbed("#cpi_lines", vg_3).catch(console.error);
