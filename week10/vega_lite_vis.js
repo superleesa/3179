3423d28c036693fd990af9cfed73f322c76222cb
Satoshi <skas0016@student.monash.edu>
2023-10-15 00:51:19 +0800

    styling finished

--- week10/vega_lite_vis.js
+++ week10/vega_lite_vis.js
@@ -1,8 +1,10 @@
 const vg_1 = "choropleth2.json";
 const vg_2 = "bump_chart.json";
 const vg_3 = "line_chart.json";
+const vg_4 = "line_chart3.json";
 
 
 vegaEmbed("#minimum_wage_map", vg_1).catch(console.error);
 vegaEmbed("#avg_wage_bump", vg_2).catch(console.error);
 vegaEmbed("#cpi_lines", vg_3).catch(console.error);
+vegaEmbed("#work_productivity_lines", vg_4).catch(console.error);
